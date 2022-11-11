832c733ffb38fd0cf2452850724f1556a97aab57
Theo Garefalakis <tgaref@gmail.com>
2022-11-11 18:50:22 +0200

    added fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,19 @@
+
+# ghcup-env
+test -f /home/tgaref/.ghcup/env;
+
+starship init fish | source
+
+function vterm_printf;
+    if begin; [  -n "$TMUX" ]  ; and  string match -q -r "screen|tmux" "$TERM"; end 
+        # tell tmux to pass the escape sequences through
+        printf "\ePtmux;\e\e]%s\007\e\\" "$argv"
+    else if string match -q -- "screen*" "$TERM"
+        # GNU screen (screen, screen-256color, screen-256color-bce)
+        printf "\eP\e]%s\007\e\\" "$argv"
+    else
+        printf "\e]%s\e\\" "$argv"
+    end
+end
+
+# set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/tgaref/.ghcup/bin $PATH # ghcup-env

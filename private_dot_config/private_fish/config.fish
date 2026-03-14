596a02683ce25a60ea059ce6558540e5ba18c9bc
Theo Garefalakis <tgaref@gmail.com>
2026-03-14 11:38:35 +0200

    Initial commit: working set of dotfiles

---
+++ private_dot_config/private_fish/config.fish
@@ -0,0 +1,24 @@
+test -f /home/tgaref/.ghcup/env;
+
+starship init fish | source
+
+function vterm_printf;
+    if begin; [  -n "$TMUX" ]  ; and  string match -q -r "screen|tmux" "$TERM"; end 
+        # tell tmux to pass the escape sequences through
+        printf "\ePtmux;\e\e]%s\007\e\\" "$argv"
+    else if string match -q -- "screen*" "$TERM"
+        # GNU screen (screen, screen-256color, screen-256color-bce)
+        printf "\eP\e]%s\007\e\\" "$argv"
+    else
+        printf "\e]%s\e\\" "$argv"
+    end
+end
+
+#if status is-interactive
+#    eval (zellij setup --generate-auto-start fish | string collect)
+#end
+
+
+# opam configuration
+source /home/tgaref/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
+~/.local/bin/mise activate fish | source

79abe9490184eea4b77632f63b5dadb524688eb2
Theo Garefalakis <tgaref@gmail.com>
2026-04-20 19:30:23 +0300

    Update fish config

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,23 +1,19 @@
-test -f /home/tgaref/.ghcup/env;
+# fish will inherit a clean PATH from ~/.profile during login
 
+# Standard Fish/Starship setup
 starship init fish | source
 
+# Only keep necessary fish-specific shell features
 function vterm_printf;
     if begin; [  -n "$TMUX" ]  ; and  string match -q -r "screen|tmux" "$TERM"; end 
-        # tell tmux to pass the escape sequences through
         printf "\ePtmux;\e\e]%s\007\e\\" "$argv"
     else if string match -q -- "screen*" "$TERM"
-        # GNU screen (screen, screen-256color, screen-256color-bce)
         printf "\eP\e]%s\007\e\\" "$argv"
     else
         printf "\e]%s\e\\" "$argv"
     end
 end
 
-#if status is-interactive
-#    eval (zellij setup --generate-auto-start fish | string collect)
-#end
-
-
-# opam configuration
+# Opam initialization for Fish-specific features (completions, hooks)
+# It only prepends the OCaml binary if it's not already in the PATH
 source /home/tgaref/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
